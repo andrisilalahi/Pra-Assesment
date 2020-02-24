eeee40cccffd0c0dac549ea220bb5af1617691f4
andrisilalahi <andrisilalahi85@gmail.com>
2020-02-24 12:06:18 +0700

    Pra Assesment

---
+++ app/src/main/java/org/d3if4042/praassment/TittleFragment.kt
@@ -0,0 +1,48 @@
+package org.d3if4042.praassment
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class Titlefragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val binding= DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater,R.layout.fragment_title,container,false)
+
+        binding.playButton.setOnClickListener { view: View -> view.findNavController().navigate(R.id.action_titlefragment2_to_gameFragment
+        )
+        }
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, view!!.findNavController())||super.onOptionsItemSelected(item)
+    }
+}
+
+class FragmentTitleBinding {
+
+}
